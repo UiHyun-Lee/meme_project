59a9f10bcc0547fada5f41a625f46af4b443d43a
uihyunlee <deutschluh@gmail.com>
2025-09-30 23:48:20 +0200

    structure changed

--- backend/evaluations/serializers.py
+++ backend/evaluations/serializers.py
@@ -4,14 +4,4 @@ from .models import Evaluation
 class EvaluationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Evaluation
-        fields = [
-            'id',
-            'meme',           # Meme FK
-            'humor_score',
-            'cultural_score',
-            'creativity_score',
-            'comment',
-            'user_id',
-            'created_at',
-        ]
-        read_only_fields = ['id', 'created_at']
+        fields = "__all__"
